39ca3d252dbf4a5fb8fe13b5ee03b642598b0c9f
andi <andi@swiftcollective.com>
2013-08-16 11:08:46 -0700

    details, js, readme

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 grid
 ====
+
+basic 12 col responsive grid with some extras
+includes a fake height equalizer and 3 up grid
+
+I included this file in my tablet break point media query
---
+++ script.js
@@ -0,0 +1,21 @@
+// put boxes behind content to help with the illusion of equal height boxes.
+// see eqHeight & eqBox in grid.scss
+// eqBox is for equal height columns with the fancy drop shadow outline
+// called on load & on resize
+namespace.equalBox = function(){
+	log("eqBox init");
+	var eqHeight;
+	if ($(window).width() > b3.smallBP){
+		$(".eqBox").each(function(){
+			eqHeight = $(this).height();
+			$(this).children(".col").each(function(index){
+				log("col" + index);
+				if ($(this).find(".boxDisplay").length < 1) {
+					$(this).prepend('<div class="boxDisplay"></div>');
+				}
+				// subtract 6px to leave room for the drop shadow bottom
+				$(this).find(".boxDisplay").height(eqHeight-6);
+			});
+		});
+	}
+}
